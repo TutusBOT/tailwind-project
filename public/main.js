3c03141e7e91ad9ea02985c1cd86ab97d4bc2827
TutusBOT <barte77pl@gmail.com>
2022-05-12 21:50:30 +0200

    working dropdown with animation

--- public/main.js
+++ public/main.js
@@ -1,1 +1,18 @@
 document.documentElement.classList.add("dark");
+const dropdown = document.getElementById("dropdown");
+const dropdownList = document.getElementById("dropdownList");
+let isOpened = false;
+dropdown.addEventListener("click", () => {
+	if (isOpened) {
+		console.log(isOpened);
+		isOpened = false;
+		dropdown.classList.add("rounded-b-xl");
+		dropdownList.classList.replace("flex", "hidden");
+	} else {
+		console.log(isOpened);
+		isOpened = true;
+		dropdown.classList.remove("rounded-b-xl");
+
+		dropdownList.classList.replace("hidden", "flex");
+	}
+});
